4f58929b43a715286095b9e2bc2ca446ce9a1d03
Jashveenraj <jashveenraj.shanmugaraj@gmail.com>
2024-12-27 13:02:48 +0530

    Tenth Commit

--- Main/src/check_char.java
+++ Main/src/check_char.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 
 class check{
     public static void character(char a){
-        if(a>=65 && a <=90){
+        if(a>=65 && a <=90){ //instead use 'A' and 'Z'
             System.out.println("Given character is an Uppercase Alphabet");
-        } else if (a>=97 && a<=122) {
+        } else if (a>=97 && a<=122) { //instead use 'a' and 'z'
             System.out.println("Given character is an LowerCase Alphabet");
-        } else if(a>=48 && a<=57){
+        } else if(a>=48 && a<=57){ //instead use '0' and '9'
             System.out.println("Given character is an Integer");
-        } else if (a>=32 && a<=47 || a>=58 && a<=64 || a>=91 && a<=96 || a>=123 && a<=126) {
+        } else if (a>=32 && a<=47 || a>=58 && a<=64 || a>=91 && a<=96 || a>=123 && a<=126) { // instead use else special character
             System.out.println("Given character is a Special Character");
         }
     }
