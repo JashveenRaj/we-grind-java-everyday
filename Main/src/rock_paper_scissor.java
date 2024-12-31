b95465394338bd083eaa59f41f52baed521402ba
Jashveenraj <jashveenraj.shanmugaraj@gmail.com>
2024-12-31 21:01:54 +0530

    14th Commit

--- Main/src/rock_paper_scissor.java
+++ Main/src/rock_paper_scissor.java
@@ -1,13 +1,26 @@
-//Write a program that simulates a rock-paper-scissors game.
-//Take input from two players as ‘R’, ‘P’, or ‘S’.
-//Use if-else determine the winner.
-
 import java.util.Scanner;
 
-class winner{
-    public static void who(char user1 , char user2){
-        if(Character.toLowerCase(user1) == 'r' && Character.toLowerCase(user2) == 'p'){
-            System.out.println("User");
+class Winner {
+    public static void who(char user1, char user2) {
+
+        user1 = Character.toLowerCase(user1);
+        user2 = Character.toLowerCase(user2);
+
+
+        if ((user1 != 'r' && user1 != 'p' && user1 != 's') ||(user2 != 'r' && user2 != 'p' && user2 != 's')) {
+            System.out.println("Invalid input! Please enter R, P, or S.");
+            return; // apparently break doesn't work inside conditional statements, so return is used.
+        }
+
+        if (user1 == user2) {
+            System.out.println("It's a tie!");
+            return;
+        }
+
+        if ((user1 == 'r' && user2 == 's') || (user1 == 'p' && user2 == 'r') || (user1 == 's' && user2 == 'p')) {
+            System.out.println("User 1 Wins!");
+        } else {
+            System.out.println("User 2 Wins!");
         }
     }
 }
@@ -15,11 +28,13 @@ class winner{
 public class rock_paper_scissor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Player 1, Enter your input (R , P, S): ");
+
+        System.out.println("Player 1, Enter your input (R, P, S): ");
         char u1 = scanner.next().charAt(0);
         System.out.println("Player 2, Enter your input (R, P, S): ");
         char u2 = scanner.next().charAt(0);
 
-        winner.who(u1,u2);
+        Winner.who(u1, u2);
+        scanner.close();
     }
 }
