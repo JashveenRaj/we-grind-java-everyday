b292ecad83fdc526bdafb33b49646224c3586a81
Jashveenraj <jashveenraj.shanmugaraj@gmail.com>
2025-01-10 12:58:05 +0530

    21st Commit

--- Main/src/Algorithms/bubble_sort.java
+++ Main/src/Algorithms/bubble_sort.java
@@ -1,11 +1,10 @@
-import java.util.ArrayList;
+package Algorithms;
 import java.util.Arrays;
 
 class bubble{
     public static int[] sort (int[] a){
-        int n = a.length;
-        for (int i = 0 ; i<n-1 ; i++){
-            for (int j = 0; j <n-1-i ; j++) {
+        for (int i = 0 ; i<a.length-1 ; i++){
+            for (int j = 0; j <a.length-1-i ; j++) {
                 if (a[j]>a[j+1]){
                     int temp = a[j];
                     a[j] = a[j+1];
