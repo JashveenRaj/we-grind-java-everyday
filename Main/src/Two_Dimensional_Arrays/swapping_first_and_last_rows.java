7aec57ae2d31aea19db5f2a068f3fd05ca904315
Jashveenraj <jashveenraj.shanmugaraj@gmail.com>
2025-01-22 08:53:20 +0530

    29th Commit

---
+++ Main/src/Two_Dimensional_Arrays/swapping_first_and_last_rows.java
@@ -0,0 +1,8 @@
+package Two_Dimensional_Arrays;
+
+public class swapping_first_and_last_rows {
+    public static void main(String[] args) {
+        int[][] a = {{1,2,3} , {4,5,6},{7,8,9}};
+
+    }
+}
