ba2b8a28858264cc763060ba3b70d5a8ccac6137
Jashveenraj <jashveenraj.shanmugaraj@gmail.com>
2025-07-24 22:19:58 +0530

    New Commit

--- Basics/printf_sample.java
+++ Basics/printf_sample.java
@@ -2,6 +2,6 @@ package Basics;
 
 public class printf_sample {
     public static void main(String[] args) {
-        System.out.printf("%.6f" , 10.0);
+        System.out.printf("%.4f" , 1.0/45.0);
     }
 }
